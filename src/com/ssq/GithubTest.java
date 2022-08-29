c8596a7299b27cc325fe6067667f5e37de4ede8c
zcp2019 <3301413033@qq.com>
2022-08-29 10:31:43 +0800

    first hot-fix commit

---
+++ src/com/ssq/GithubTest.java
@@ -0,0 +1,13 @@
+package com.ssq;
+
+/**
+ * @author :zh
+ * @PACKAGE_NAME :com.ssq
+ * @NAME : GithubTest
+ * @date :2022Äê08ÔÂ29ÈÕ10:28
+ */
+public class GithubTest {
+    public static void main(String[] args) {
+        System.out.println("hot-fix");
+    }
+}
